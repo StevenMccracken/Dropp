8905672166e7983c0ff04994d375d8337c532d54
Steven McCracken <stevenamccracken@gmail.com>
2017-02-20 14:24:40 -0800

    Added basic button to app but the app crashes upon opening. Added a basic get handler for the server

--- Dropp/Dropp/Dropp/ViewController.swift
+++ Dropp/Dropp/Dropp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //Mark: Actions
+    @IBAction func tapMeButton(sender: UIButton) {
+        contactServer()
+    }
+    
+    func contactServer() {
+        print("Button Pressed")
+        
+        var bodyData = "data=something"
+        var url: NSURL = NSURL(string: "localhost:3000")!
+        var request:NSMutableURLRequest = NSMutableURLRequest(url:url as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = bodyData.data(using: String.Encoding.utf8);
+        
+        NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: OperationQueue.main)
+        {
+            (response, data, error) in
+            print(response)
+            
+        }
+    }
 }
-
--- Dropp/index.js
+++ Dropp/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.post('/', function(req, res) {
+  const shit = req.body;
+  res.send('heyo');
+})
+
 app.get('/', (request, response) => {
   response.send('Hello from Express!');
 });
