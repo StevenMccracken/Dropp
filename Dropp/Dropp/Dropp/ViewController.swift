3acfc1c9ea6d223f093f34d591452a69156f9a1f
Steven McCracken <stevenamccracken@gmail.com>
2017-02-20 13:43:37 -0800

    Set up skeleton iOS project

---
+++ Dropp/Dropp/Dropp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dropp
+//
+//  Created by Steven McCracken on 2/20/17.
+//  Copyright © 2017 Group B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
