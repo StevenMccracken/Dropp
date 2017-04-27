bcc82f5fa10c2b2976858433f0e3452b618aa433
Rich Cherngchaosil <richer_po@hotmail.com>
2017-04-27 16:00:49 -0700

    Update unit Test

--- Dropp/test/spec/index.spec.js
+++ Dropp/test/spec/index.spec.js
@@ -1,9 +1,13 @@
 
 var request = require("request");
 var server = require("../../server.js");
-var base_url = "http://localhost:3000"
+var base_url = "http://localhost/"
 // port 80 require sudo accesss, in order for the test to be done on travis, we have to user other port
 // Server will check for TEST env variable and adjust the port according to environment
+
+if(process.env.TEST){
+	base_url = "http://localhost:3000"
+}
 
 describe("Hellow World Server", function(){
 	describe("GET /", function(){
