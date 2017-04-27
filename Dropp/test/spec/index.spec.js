9aa0c7b3ff137ad143a1cfad55b8c33d3c085eb4
Rich Cherngchaosil <richer_po@hotmail.com>
2017-04-27 16:14:50 -0700

    Unit test for authentication

--- Dropp/test/spec/index.spec.js
+++ Dropp/test/spec/index.spec.js
@@ -6,10 +6,10 @@ var base_url = "http://localhost/"
 // Server will check for TEST env variable and adjust the port according to environment
 
 if(process.env.TEST){
-	base_url = "http://localhost:3000"
+	base_url = "http://localhost:3000/"
 }
 
-describe("Hellow World Server", function(){
+describe("Server", function(){
 	describe("GET /", function(){
 		it("returns status code 200", function(done){
 			request.get(base_url, function(error, response, body){
@@ -21,10 +21,35 @@ describe("Hellow World Server", function(){
 		it("returns Hello World", function(done){
 			request.get(base_url, function(error, response, body){
 				expect(body).toBe('{"message":"This is the REST API for Dropp"}');
-				server.closeServer();
+				
 				done();
 			})
 		})
 
 	})
+
+	describe('Authenticate', function() {
+
+        var params = null;
+        beforeEach(function() {
+            params = {
+                url: base_url + "authenticate",
+                form: {
+                    username: 'rich',
+                    password: '1234',
+                    email: 'test@gmail.com',
+                }
+            };
+        });
+
+        it('returns a 200 OK on valid user', function(done) {
+            request.post(params, function(err, resp, body) {
+                var data = JSON.parse(body);
+                // expect(resp.statusCode).toEqual(200);
+                expect(data.success.token).toBeDefined();
+                server.closeServer();
+                done();
+            });
+        });
+    });
 })
